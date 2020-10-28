038b104ecd66a71b8e0a5ad6c222901d959d714f
LighghtEeloo <alex.yc@sjtu.edu.cn>
2020-10-29 01:09:48 +0800

    Staticmethod. Depth system added.

--- note.py
+++ note.py
@@ -2,54 +2,23 @@
 # !/usr/bin/python3
 
 class Elt():
-    def __init__(self, name: str=None, index: int=None, content: str=None):
+    def __init__(self, name: str, auto: str=None, index: int=None, content: str=None):
+        if auto is None:
+            if ti := name.find(':') != -1:
+                auto = name[ti+1:]
+                name = name[:ti+1]
         self.name = name
+        self.auto = auto
         self.index = index
         self.content = content
+        # print("::", (name, auto))
     def __repr__(self) -> str:
         return f'{self.name}{": " + self.content if self.content else ""}'
-
-def search(raw: str, tag: str="<>") -> tuple:
-    """
-    raw: str, tag: two chars
-    search("raw <string>", "<>") -> ("raw <>", "string")
-    search("raw <string> is <bad>", "<>") -> ("raw <> is <bad>", "string")
-    search("raw <> is <bad>", "<>") -> ("raw <> is <>", "bad")
-    search("raw <> is <>", "<>") -> ("raw <> is <>", None)
-    """
-    cur = -1
-    for i in range(len(raw)):
-        if cur == -1 and raw[i] == tag[0]:
-            cur = i
-        elif cur != -1 and raw[i] == tag[1]:
-            if cur != i - 1:
-                return "".join((raw[0:cur+1], raw[i:])), raw[cur+1:i]
-            else:
-                cur = -1
-    return raw, None
-
-def search_iter(raw: str, tag: str="<>") -> tuple:
-    """
-    search_iter("raw <string> is <bad>", "<>") -> ("raw <> is <>", ["string", "bad"])
-    search("raw <> is <>", "<>") -> ("raw <> is <>", [])
-    """
-    lst = []
-    while True:
-        raw, res = search(raw, tag)
-        if res is None:
-            break
-        else:
-            lst.append(res)
-    return raw, lst
-
-def elementize(raw: str, elements: list=None, tag: str="<>"):
-    raw, lst = search_iter(raw, tag)
-    return raw, list(map(Elt,lst))
 
 class Pat():
     def __init__(self, raw: str, attr: list=None, tag: str="<>"):
         if attr is None:
-            self.raw, self.attr = elementize(raw, attr, tag)
+            self.raw, self.attr = self.elementize(raw, attr, tag)
         else:
             self.raw = raw
             self.attr = attr
@@ -59,6 +28,49 @@ class Pat():
         for i in range(len(self.attr)-1,-1,-1):
             raw_lst.insert(i+1,f"<{self.attr[i]}>")
         return "".join(raw_lst)[2:-2]
+    @staticmethod
+    def search(raw: str, tag: str="<>") -> tuple:
+        """
+        raw: str, tag: two chars
+        search("raw <string>", "<>") -> ("raw <>", "string")
+        search("raw <string> is <bad>", "<>") -> ("raw <> is <bad>", "string")
+        search("raw <> is <bad>", "<>") -> ("raw <> is <>", "bad")
+        search("raw <> is <>", "<>") -> ("raw <> is <>", None)
+        """
+        cur = -1
+        dep = 0
+        for i in range(len(raw)):
+            if raw[i] == tag[0]:
+                dep += 1
+                if cur == -1:
+                    cur = i
+            elif cur != -1 and raw[i] == tag[1]:
+                dep -= 1
+                if dep == 0:
+                    if cur != i - 1:
+                        return "".join((raw[0:cur+1], raw[i:])), raw[cur+1:i]
+                    else:
+                        cur = -1
+        return raw, None
+    @staticmethod
+    def search_iter(raw: str, tag: str="<>") -> tuple:
+        """
+        search_iter("raw <string> is <bad>", "<>") -> ("raw <> is <>", ["string", "bad"])
+        search("raw <> is <>", "<>") -> ("raw <> is <>", [])
+        """
+        lst = []
+        while True:
+            raw, res = Pat.search(raw, tag)
+            if res is None:
+                break
+            else:
+                lst.append(res)
+        return raw, lst
+    @staticmethod
+    def elementize(raw: str, elements: list=None, tag: str="<>"):
+        raw, lst = Pat.search_iter(raw, tag)
+        # print(f'>> {raw}, {list(map(Elt,lst))}')
+        return raw, list(map(Elt,lst))
 
 
 class Session():
@@ -82,4 +94,4 @@ class Session():
 
 if __name__ == "__main__":
     session = Session()
-    # print(Pat("string is <bad>, really, <comment>."))
+    # print(Pat("string is <<bada> Anna>, really, <comment>."))
