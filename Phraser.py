2a6bd87b826cb1c89a9705f65813e9373298bd10
LighghtEeloo <alex.yc@sjtu.edu.cn>
2020-10-31 22:04:19 +0800

    Head info added.

--- Phraser.py
+++ Phraser.py
@@ -11,13 +11,14 @@ class Phraser():
         """
         args
         """
-        self.headed = False
+        self.headed = False # Todo: maintain it.
         self.head = [
-            "Date & time:",
-            "Student:",
-            "Consultant:",
-            "Session note:"
+            Pat("Date & time: <date & time>"),
+            Pat("Student: <student>"),
+            Pat("Consultant: <consultant>"),
+            Pat("Session note: ")
         ]
+        self.phrased = False # Todo: maintain it.
         self.phrases: list(Pat) = [
             # Pat("The student came in with <item> <status>."),
             # Pat("<gender:his> <aspect> is <comment>"),
@@ -29,6 +30,12 @@ class Phraser():
         self.session.update(schema.content_fixed())
         return self
     def apply_schema(self):
+        if not self.headed:
+            for head in self.head:
+                for elt in head.attr:
+                    if elt.name in self.session.keys():
+                        elt.content = self.session[elt.name]
+            self.headed = True
         for i in range(len(self.phrases)):
             for j in range(len(self.phrases[i].attr)):
                 elt = self.phrases[i].attr[j]
@@ -42,11 +49,18 @@ class Phraser():
         if verbose:
             print("----------(Schema)----------")
             print(Schema.dumps(self.session))
+            if self.headed:
+                print("-----------(Head)-----------")
+                for head in self.head:
+                    print(head)
             if (len(self.phrases)):
                 print("----------(Phrase)----------")
                 print(self.phrases[0])
             print("------------(End)------------")
         else:
+            if self.headed:
+                for head in self.head:
+                    print(head)
             print(self.phrases[0])
     def input(self, sentence: str):
         pass
--- note.py
+++ note.py
@@ -3,10 +3,8 @@ from Phraser import *
 
 if __name__ == "__main__":
     schema = Schema(default_dir_schema).load()
-    # schema.input()
-    # schema.dump()
+    schema.input()
+    schema.dump()
     phr = Phraser().add_schema(schema)
-    print(phr.phrases)
     phr.apply_schema()
-    print(phr.phrases)
-    phr.output(verbose=True)
+    phr.output(verbose=False)
